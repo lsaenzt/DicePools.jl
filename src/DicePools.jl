476e8254e7ce9b69b42067173c4953f21f62a36b
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2021-11-10 22:25:43 +0100

    Clean main module

--- src/DicePools.jl
+++ src/DicePools.jl
@@ -20,17 +20,3 @@ include("DiceExamples.jl")
 include("Utils.jl")
 
 end # module
-
-#= ToDO List
-    1.- [DONE]Subsract dice -> add method -(DiceProbabilities), -(DiceProbabilities,DiceProbabilities), 
-    2.- [DONE] Also, +(DiceProbabilities,DiceProbabilities...), +(StandardDice, Int), *(Int,Dice)
-    3.- [DONE] macro @roll 3d6+1 -> Not useful if 1 & 2 work
-    4.- [DONE] Solve sorting results when substracting dice
-    5.- [DONE] Solve minus dice (-3d6)
-    6.- reroll numeric
-    7.- exploding dice (~ reroll 'x' times)
-    8.- hybrid dice (e.g. The one ring: d10 plus symbols)
-    9.- double target (e.g. Conand20: 1 success or 2 successes)
-    10.- [DONE] Groupby combined dice
-    11.- [WIP] docs
-=#
