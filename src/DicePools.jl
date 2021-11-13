2e31470139e4c752751ba72ab9fccdc5657d6224
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2021-11-13 15:45:31 +0100

    PrettyTables out

--- src/DicePools.jl
+++ src/DicePools.jl
@@ -3,7 +3,6 @@ module DicePools
 using Combinatorics
 using OrderedCollections
 using Tables
-using PrettyTables
 
 export roll, customroll, reroll, sampleroll, drop, takemid, beattarget, rollunder, pool
 export d4, d6, d8, d10, d12, d20, d100, fudge, MY0_Skill, MY0_Eq, MY0_Attr, Conan_Dmg,
