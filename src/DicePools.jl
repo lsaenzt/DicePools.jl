2a8d7b037648356de0eec93cfe521c88f05a31ba
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2023-07-29 18:18:40 +0200

    Added precompile workload

--- src/DicePools.jl
+++ src/DicePools.jl
@@ -3,6 +3,7 @@ module DicePools
 using Combinatorics
 using OrderedCollections
 using Tables
+using PrecompileTools
 
 export roll, customroll, reroll, sampleroll, highest, drop, takemid, beattarget, rollunder, pool
 export d4, d6, d8, d10, d12, d20, d100, fudge, MY0_Skill, MY0_Eq, MY0_Attr, Conan_Dmg,
@@ -19,10 +20,15 @@ include("Mechanics.jl")
 include("DiceExamples.jl")
 include("Utils.jl")
 
-let 
-    roll(1,d4)
-    highest(1,d4)
-    drop(1,d4)
+@setup_workload begin
+    # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
+    # precompile file and potentially make loading faster.
+    @compile_workload begin
+        roll(1,d4)
+        highest(1,d4)
+        drop(1,d4)
+        takemid(1,d6)
+    end
 end
 
 end # module
